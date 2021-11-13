5ad2e6d19f1701f8a5f98c6e97e7daddd2a7f0e5
aitorzatica <aitor.zatica@opendeusto.es>
2021-11-13 21:45:01 +0100

    añado dependencia Guava de Google para Split más avanzados y formateo y obtengo la primera parte de los datos clave valor de la consulta avanzada de peliculas en forma de mapa

--- src/main/java/backend/gateway/Gateway.java
+++ src/main/java/backend/gateway/Gateway.java
@@ -1,8 +1,7 @@
 package backend.gateway;
 import backend.gateway.IGateway;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -14,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.fasterxml.jackson.databind.JsonSerializable;
+import com.google.common.base.Splitter;
 import org.json.simple.JSONArray;
 import org.json.*;
 import org.json.simple.JSONObject;
@@ -71,6 +71,10 @@ public class Gateway implements IGateway {
         for(int i=0;i<separado.length;i++) {
             String[] obtId = separado[i].split("\"");
             separado[i] = obtId[0];
+            if(i==1)
+            {
+                break;
+            }
             //System.out.println(separado[i]+"\n");
         }
         System.out.println("Separado (0)" + separado[1]);
@@ -80,12 +84,51 @@ public class Gateway implements IGateway {
     public String obtenerDatosPelicula(String stringobj) //esto quizas no iria en esta clase
     {
         //AQUI SE TRATARAN LOS DATOS QUE VIENEN DE LA SEGUNDA CONSULTA
-        System.out.println(stringobj);
+        //System.out.println(stringobj);
+
+        //String test = "pet:cat::car:honda::location:Japan::food:sushi";
+        //Map<String, String> map = Splitter.on( "::" ).withKeyValueSeparator( ':' ).split( test );
+
+        //System.out.println("UNOOOO");
+        //Iterable<String> iter = Splitter.on("\",").omitEmptyStrings().split(stringobj);
+
+        System.out.println("\n\nPRIMERA PARTE\n\n");
+        String[] splitPrimero = stringobj.split(",\"Ratings");
+        //System.out.println(splitPrimero[0].substring(1));
+
+
+        System.out.println(Splitter.on("\",\"").omitEmptyStrings().withKeyValueSeparator("\":\"").split(splitPrimero[0].substring(0, splitPrimero[0].length() - 1).substring(2)));
+
+        System.out.println("\n\nSEGUNDA PARTE\n\n");
+        System.out.println(splitPrimero[1].substring(splitPrimero[1].indexOf("Runtime")-1));
+        /*
+        System.out.println("UNOOOO2");
+        String[] splitSegundo;
+        splitSegundo = splitPrimero[1].split(",\"Ratings");
+        System.out.println(splitSegundo[1]);
+
+        System.out.println("UNOOOO3");
+        System.out.println(Splitter.on(',').omitEmptyStrings().withKeyValueSeparator(':').split(splitPrimero[0]));
+
+         */
+
+        /*
+        Splitter.on('[').split(stringobj);
+        iter.forEach();
+        System.out.println("DOSSSS");
+        //Map<String, String> map1 = Splitter.on(',').omitEmptyStrings().limit(14).withKeyValueSeparator(':').split(stringobj);
+        System.out.println(Splitter.on(',').omitEmptyStrings().limit(14).withKeyValueSeparator(':').split(stringobj));
+
+        System.out.println("TRESSSS");
+        System.out.println(Splitter.on(',').omitEmptyStrings().withKeyValueSeparator(':').split(stringobj));
+
+         */
+
         return "";
     }
 
     @Override
-    public ArrayList<HashMap<String, Object>> exportPeliculaGenerico(String pelicula) throws Exception {
+    public ArrayList<HashMap<String, Object>> exportPeliculaGenerico(String pelicula) throws Exception { //sera simplemente hashmap porque se devolvera solo 1 pelicula
         String url="https://www.omdbapi.com/?s="+pelicula+"&apikey=3ae17be9";
         this.url=url;
         accessPoint();
@@ -112,6 +155,7 @@ public class Gateway implements IGateway {
         accessPoint();
         String respuesta ="";
         respuesta=makeGetRequest();
+        obtenerDatosPelicula(respuesta);
         //aqui se trata la segunda respuesta
         return null;
     }
